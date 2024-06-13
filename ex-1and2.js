2dcfa3e7b0a7f37f159a3c5de7cd898b8ea7be2b
konthron gongmanee <konthron.ant@gmail.com>
2024-06-13 23:13:34 +0700

    feat:value variable assignment finish

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter"
+let surname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const amount = "15000";
+const openRestaurant = "1500";
+
+
+
+
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 Country Rd #4Meeker, Colorado(CO), 81641";
+amount = "5000"
+openRestaurant = "2500";
+
